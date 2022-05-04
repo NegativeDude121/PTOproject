bb5c96c02bbcf15dbd6189f67bb895853d8f47df
NegativeDude121 <tymeczek@vp.pl>
2022-05-04 18:16:38 +0200

    saveMap
    made a map and a generator script for the maze

---
+++ PTOproject/Assets/Scripts/ColorToPrefab.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Umo¿liwia edytowanie obiektu klasy ColorToPrefab z poziomu inspektora w Unity
+[System.Serializable]
+public class ColorToPrefab
+{
+    public Color color;
+    public GameObject prefab;
+}
